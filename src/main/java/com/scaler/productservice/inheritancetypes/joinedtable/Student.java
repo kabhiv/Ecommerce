124350677407551ae9b4156e2c35d741dc28ccce
abhiani99 <87971723+abhiani99@users.noreply.github.com>
2024-12-12 17:40:41 +0530

    inheritance

---
+++ src/main/java/com/scaler/productservice/inheritancetypes/joinedtable/Student.java
@@ -0,0 +1,16 @@
+package com.scaler.productservice.inheritancetypes.joinedtable;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "jt_student")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Student extends User {
+    private String Batch;
+
+}
---
+++ src/main/java/com/scaler/productservice/inheritancetypes/tableperclass/Instructor.java
@@ -0,0 +1,13 @@
+package com.scaler.productservice.inheritancetypes.tableperclass;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "tpc_instructor")
+public class Instructor extends User {
+    private String subject;
+}
